259ab0d50c09d35028e8ae7862286cd16912b591
RobsonFe <robson_20ferreira@hotmail.com>
2024-12-27 14:17:26 -0300

    adiciona configuração do TypeORM e módulo de configuração global; cria arquivo .env.example

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,32 @@ import { CompanyModule } from './modules/company/module/company.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-    imports: [EmployeeModule, CompanyModule],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => ({
+                type: 'postgres',
+                host: configService.get('DB_HOST'),
+                port: configService.get<number>('DB_PORT'),
+                username: configService.get('DB_USERNAME'),
+                password: configService.get('DB_PASSWORD'),
+                database: configService.get('DB_DATABASE'),
+                autoLoadEntities: true, // Carrega automaticamente as entidades
+                entities: [__dirname + '/**/*.entity{.ts,.js}'],
+                synchronize: true, // não pode ser usado em produção, apenas para desenvolvimento.
+            }),
+        }),
+        EmployeeModule,
+        CompanyModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
