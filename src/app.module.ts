0aa48a06f042eb04addb5d264c01797adb85ee12
RobsonFe <robson_20ferreira@hotmail.com>
2024-12-26 22:27:46 -0300

    adiciona módulo, controlador, serviço e repositório da empresa; habilita CORS e define prefixo global da API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
+import { CompanyModule } from './modules/company/company.module';
+import { CompanyService } from './modules/company/service/company.service';
+import { CompanyController } from './modules/company/controller/company.controller';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [CompanyModule],
+    controllers: [CompanyController, AppController],
+    providers: [CompanyService, AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     await app.listen(process.env.PORT ?? 8080);
+
+    app.enableCors({
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        allowedHeaders: 'Content-Type, Accept, Authorization',
+    });
+
+    app.setGlobalPrefix('api/v1', {
+        exclude: ['/'],
+    });
 }
 bootstrap();
