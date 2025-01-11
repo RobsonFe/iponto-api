a17f00f69897870302b0d359ab3b499f6b051c60
RobsonFe <robson_20ferreira@hotmail.com>
2025-01-11 15:29:23 -0300

    adiciona tratamento de erros nas operações do controlador de funcionários; implementa exceções internas para melhorar a robustez

--- src/modules/employee/controller/employee.controller.ts
+++ src/modules/employee/controller/employee.controller.ts
@@ -27,30 +27,55 @@ export class EmployeeController {
     }
 
     @Get('list')
+    @HttpCode(HttpStatus.OK)
     async findAll(
         @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
         @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number
     ): Promise<EmployeeEntity[]> {
-        return this.service.findAll(page, limit);
+        try {
+            return this.service.findAll(page, limit);
+        } catch (error) {
+            throw new InternalServerErrorException(HttpCode(HttpStatus.INTERNAL_SERVER_ERROR));
+        }
     }
 
     @Get('find/:id')
+    @HttpCode(HttpStatus.OK)
     async findById(@Query('id') id: string): Promise<EmployeeEntity | null> {
-        return this.service.findById(id);
+        try {
+            return this.service.findById(id);
+        } catch (error) {
+            throw new InternalServerErrorException(HttpCode(HttpStatus.INTERNAL_SERVER_ERROR));
+        }
     }
 
     @Patch('update/:id')
+    @HttpCode(HttpStatus.OK)
     async update(@Param('id') id: string, @Body() employee: CreateEmployeeDto): Promise<EmployeeEntity | null> {
-        return this.service.update(id, employee);
+        try {
+            return this.service.update(id, employee);
+        } catch (error) {
+            throw new InternalServerErrorException(HttpCode(HttpStatus.INTERNAL_SERVER_ERROR));
+        }
     }
 
     @Delete('delete/:id')
+    @HttpCode(HttpStatus.NO_CONTENT)
     async delete(@Param('id') id: string): Promise<void> {
-        return this.service.delete(id);
+        try {
+            return this.service.delete(id);
+        } catch (error) {
+            throw new InternalServerErrorException(HttpCode(HttpStatus.INTERNAL_SERVER_ERROR));
+        }
     }
 
     @Get('count')
+    @HttpCode(HttpStatus.OK)
     async count(): Promise<number> {
-        return this.service.count();
+        try {
+            return this.service.count();
+        } catch (error) {
+            throw new InternalServerErrorException(HttpCode(HttpStatus.INTERNAL_SERVER_ERROR));
+        }
     }
 }
